e16186fe7869aa9e92f8d4ff3ae1acd578f103b2
valcode84 <stefanovalentini04@gmail.co>
2022-05-27 11:06:15 +0100

    first commit movie db

---
+++ movies.js
@@ -0,0 +1,71 @@
+export const movies = [
+    {
+      id: 1,
+      director: "Spike Lee",
+      title: "Do the right thing",
+      release: "23/06/1989",
+    },
+    {
+      id: 2,
+      director: "Christopher Nolan",
+      title: "Interstellar",
+      release: "07/11/2014",
+    },
+    {
+      id: 3,
+      director: "Quentin Tarantino",
+      title: "Pulp fiction",
+      release: "21/10/1994",
+    },
+    {
+      id: 4,
+      director: "Christopher Nolan",
+      title: "Inception",
+      release: "08/07/2010",
+    },
+    
+    {
+        id: 5,
+        director: "Martin Scorsese",
+        title: "Good Fellas",
+        release: "26/10/90",
+      },
+
+      {
+        id: 6,
+        director: "Frank Darabont",
+        title: "The shawshank redemption",
+        release: "17/02/95",
+      },
+
+      {
+        id: 7,
+        director: "Francis Ford Coppola",
+        title: "The god father",
+        release: "24/08/72",
+      },
+
+      {
+        id: 8,
+        director: "Steaven Spielberg",
+        title: "Shindler's list",
+        release: "18/02/1994",
+      },
+
+      {
+        id: 9,
+        director: "David Fincher",
+        title: "Fight Club",
+        release: "12/11/1999",
+      },
+
+
+      {
+        id: 10,
+        director: "Steven Spielberg",
+        title: "Saving private Ryan",
+        release: "11/09/1998",
+      },
+      
+  ];
+  
